7de7e1fb76ba5d300304aa29014e2752725e67b7
shubhamp19 <Insp!re@1903>
2026-03-09 20:07:28 +0530

    commit spring POC

---
+++ src/main/java/com/shubham/springdemo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.shubham.springdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello this only demo of Spring Application for dev-ops";
+    }
+}
